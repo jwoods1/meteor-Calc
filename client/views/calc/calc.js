2f4f9b4d4242fdd606cbdba0dc9466b95766e063
b87775 <jwoods1@fullsail.edu>
2014-05-04 00:14:18 -0600

    comments calculator

--- client/views/calc/calc.js
+++ client/views/calc/calc.js
@@ -1,4 +1,6 @@
+/* Uses meteor template to listen for inputs from calc buttons */
 Template.calc.events({
+  //all number and symbols
   'click .calc-btn': function(event,template) {
     var val = $(event.currentTarget).text();
     if($(".display").attr("value") == 0){
@@ -8,10 +10,10 @@ Template.calc.events({
       var multiVal = displayVal+=val;
       $(".display").attr("value", multiVal);
     }
-  },
+  },// clear button to reset value
   'click .clear': function(event,template) {
     $(".display").attr("value","0");
-  },
+  },// eval the value attr of the display ! 
   'click .equals': function(event,template) {
     var equals = eval($(".display").attr("value"));
     $(".display").attr("value",equals);
